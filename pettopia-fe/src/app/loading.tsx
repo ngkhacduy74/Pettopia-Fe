cef68bc930879778e6d7d4213b9a9da25772c651
PhucNguyen-Elfa <sonphuc161103@gmail.com>
2025-10-02 22:00:21 +0700

    Add loading.tsx

---
+++ pettopia-fe/src/app/loading.tsx
@@ -0,0 +1,1 @@
+export default function Home() { return (<main style={{ textAlign: "center", marginTop: "50px" }}> <h1>Loading Test</h1> <div style={{ width: "200px", height: "100px", margin: "20px auto", backgroundImage: "url(./sampleimg/cat.gif)", backgroundSize: "cover", borderRadius: "12px", }} ></div> </main>); }
