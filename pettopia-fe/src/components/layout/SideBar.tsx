42c909a229071c3b26c8549f65164213d5b9944d
Trần Nam Anh <onlynanh@gmail.com>
2025-12-16 00:20:14 +0700

    Sửa lại premium

--- pettopia-fe/src/components/layout/SideBar.tsx
+++ pettopia-fe/src/components/layout/SideBar.tsx
@@ -359,12 +359,7 @@ export default function UserNavbar({ setShowSearch, showSearch }: UserNavbarProp
               <div className="space-y-1">
                 <Link href="/user/pet/list">
                   <button
-<<<<<<< Updated upstream
-                    className={`w-full flex ${isSidebarCollapsed ? 'justify-center' : 'items-center'} gap-3 px-3 py-2 rounded-lg text-sm transition-colors cursor-pointer ${
-                      pathname === '/user/pet/list'
-=======
-                    className={`w-full flex ${isSidebarCollapsed ? 'justify-center' : 'items-center'} gap-3 px-3 py-2 rounded-lg text-sm transition-colors ${pathname === '/user/pet/list'
->>>>>>> Stashed changes
+                    className={`w-full flex ${isSidebarCollapsed ? 'justify-center' : 'items-center'} gap-3 px-3 py-2 rounded-lg text-sm transition-colors cursor-pointer${pathname === '/user/pet/list'
                         ? 'bg-gradient-to-r from-teal-500 to-cyan-600 text-white shadow-sm'
                         : 'text-gray-700 hover:bg-gray-100'
                       }`}
